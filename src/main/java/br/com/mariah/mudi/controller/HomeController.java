0d7922c6643ffae7fcebdf60231d538c0fb3dd91
Matheus2021s <matheusmartinsdacosta21@gmail.com>
2021-08-23 01:22:07 -0300

    Implementing spring security to manage form login using database. This kind of implementation permits spring to use a form login to create a session to user

--- src/main/java/br/com/mariah/mudi/controller/HomeController.java
+++ src/main/java/br/com/mariah/mudi/controller/HomeController.java
@@ -1,21 +1,40 @@
 package br.com.mariah.mudi.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.mariah.mudi.model.StatusPedido;
 import br.com.mariah.mudi.repository.PedidoRepository;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
 	
-	@GetMapping("/home")
-	public String home(Model model) {
-		model.addAttribute("pedidos", this.pedidoRepository.findAll());
+	@GetMapping
+	public String home(Model model, Principal principal) {
+		model.addAttribute("pedidos", this.pedidoRepository.findAllByUsuario(principal.getName()));
 		return "home";
+	}
+	
+	@GetMapping("/{status}")
+	public String porStatus(@PathVariable("status")String status, Model model) {
+		model.addAttribute("pedidos", this.pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase())));
+		model.addAttribute("status",status);
+		return "home";
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirect:/home";
 	}
 }
--- src/main/java/br/com/mariah/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/mariah/mudi/repository/PedidoRepository.java
@@ -1,11 +1,21 @@
 package br.com.mariah.mudi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.mariah.mudi.model.Pedido;
+import br.com.mariah.mudi.model.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByStatus(StatusPedido status);
+
+	@Query("select p from Pedido p join p.user u where u.username = :username ")
+	List<Pedido> findAllByUsuario(@Param("username")String username);
 
 }
